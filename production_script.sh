ec6c75dd681774c894f56cc4cc36635e0a7eb0b6
Rafy Benjamin <rafy.amgadbenjamin@gmail.com>
2020-12-10 17:52:53 +0200

    fixing some bugs

--- production_script.sh
+++ production_script.sh
@@ -26,6 +26,14 @@ systemctl restart redis.service
 systemctl unmask influxdb.service
 systemctl start influxdb
 
+git clone -b development_guni https://github.com/OmarElawady/vdc-logger.git
+cd vdc-logger
+
+# Update the Caddy service with the domain
+sed -i "s/LOCAL_HOST/$1/g" Caddyfile
+
+pip3 install -r requirements.txt
+
 # Install Caddy server
 echo "deb [trusted=yes] https://apt.fury.io/caddy/ /" \  | sudo tee -a /etc/apt/sources.list.d/caddy-fury.list
 
@@ -35,20 +43,16 @@ apt install caddy -y
 systemctl stop caddy.service
 systemctl disable caddy.service
 
-git clone -b development_guni https://github.com/OmarElawady/vdc-logger.git
-pip3 install -r vdc-logger/requirements.txt
-cd vdc-logger
-
 # Move our redis service to the systemd services
 cp services/tf-redis-listener.service /etc/systemd/system
 cp services/tf-logging-server.service /etc/systemd/system
 cp services/tf-caddy-server.service /etc/systemd/system
-
-# Update the Caddy service with the domain
-sed -i 's/LOCAL_HOST/$1/g' Caddyfile
 
 # Reload and start the services
 systemctl daemon-reload
 systemctl start tf-redis-listener.service
 systemctl start tf-logging-server.service
 systemctl start tf-caddy-server.service
+systemctl enable tf-redis-listener.service
+systemctl enable tf-logging-server.service
+systemctl enable tf-caddy-server.service
