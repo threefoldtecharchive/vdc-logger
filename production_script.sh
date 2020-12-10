853cd3926e2286052160cb7834d7bca12a6c05d8
Rafy Benjamin <rafy.amgadbenjamin@gmail.com>
2020-12-10 15:47:27 +0200

    adding production script and services needed for logging machine

---
+++ production_script.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+apt-get update
+apt-get install -y apt-transport-https
+apt-get install -y software-properties-common wget
+wget -q -O - https://packages.grafana.com/gpg.key | sudo apt-key add -
+# Adding Grafana
+echo "deb https://packages.grafana.com/oss/deb stable main" | tee -a /etc/apt/sources.list.d/grafana.list
+apt-get update
+apt-get install grafana -y
+
+# Start Grafana
+systemctl daemon-reload
+systemctl start grafana-server
+systemctl enable grafana-server.service
+
+# Adding influx repo
+wget -qO- https://repos.influxdata.com/influxdb.key | sudo apt-key add -
+source /etc/lsb-release
+echo "deb https://repos.influxdata.com/${DISTRIB_ID,,} ${DISTRIB_CODENAME} stable" | tee -a /etc/apt/sources.list.d/influxdb.list
+
+apt-get update
+apt-get install git python3-pip redis-server influxdb -y
+sed -i 's/supervised no/supervised systemd/g' /etc/redis/redis.conf
+systemctl restart redis.service
+systemctl unmask influxdb.service
+systemctl start influxdb
+
+# Install Caddy server
+echo "deb [trusted=yes] https://apt.fury.io/caddy/ /" \  | sudo tee -a /etc/apt/sources.list.d/caddy-fury.list
+
+apt update
+apt install caddy -y
+
+# git clone https://github.com/OmarElawady/vdc-logger.git
+pip3 install -r vdc-logger/requirements.txt
+cd vdc-logger
+
+# Move our redis service to the systemd services
+cp services/tf-redis-listener.service /etc/systemd/system
+cp services/tf-logging-server.service /etc/systemd/system
+
+# Reload and start the services
+systemctl daemon-reload
+systemctl start tf-redis-listener.service
+systemctl start tf-logging-server.service
--- redislistener.py
+++ redislistener.py
@@ -5,38 +5,32 @@ import config
 
 
 def process_message(message, db_connection):
-    channel = message.get('channel').decode()
-    message_data = message.get('data').decode()
-    can = channel[4:] 
-    vdc_name, rest = can.split('_', 1)
-    tname, stream = rest.rsplit('-', 1)
+    channel = message.get("channel").decode()
+    message_data = message.get("data").decode()
+    can = channel[4:]
+    vdc_name, rest = can.split("_", 1)
+    tname, stream = rest.rsplit("-", 1)
     points = [
         {
             "measurement": "logs",
-            "tags": {
-                "tname": tname,
-                "vdc_name": vdc_name,
-                "stream": stream
-            },
-            "fields": {  
-                "message": message_data
-            }
-        },
+            "tags": {"tname": tname, "vdc_name": vdc_name, "stream": stream},
+            "fields": {"message": message_data},
+        }
     ]
     write_messages(db_connection, points)
 
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     db_connection = connect_influx()
-    r = redis.Redis(host='localhost', port=int(config.REDIS_PORT), password=config.REDIS_PASSWORD, db=0)
+    r = redis.Redis(host="localhost", port=int(config.REDIS_PORT), db=0)
+    # r = redis.Redis(host="localhost", port=int(config.REDIS_PORT), password=config.REDIS_PASSWORD, db=0)
 
     p = r.pubsub()
-    p.psubscribe('vdc_*_*-*') # edit to match vdc only
+    p.psubscribe("vdc_*_*-*")  # edit to match vdc only
     while True:
         x = p.get_message()
         while x:
-            if x['type'] == 'pmessage':
+            if x["type"] == "pmessage":
                 process_message(x, db_connection)
                 print(x)
             x = p.get_message()
