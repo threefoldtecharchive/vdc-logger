9beca9a091b1d18dd735b1dcd1eef86b573c77b8
Rafy Benjamin <rafy.amgadbenjamin@gmail.com>
2020-12-13 17:22:15 +0200

    udpate caddyfile and create the databases

--- logger/db.py
+++ logger/db.py
@@ -6,6 +6,7 @@ from flask.cli import with_appcontext
 from influxdb import InfluxDBClient
 from flask import current_app
 
+
 def get_db():
     return InfluxDBClient(host=current_app.config['DB_HOST'], port=current_app.config['DB_PORT'], username=current_app.config['DB_USERNAME'], password=current_app.config['DB_PASSWORD'])
 
@@ -13,8 +14,13 @@ def close_db(e=None):
     g.pop('db', None)
 
 def init_db():
-    pass
+    db_client = get_db()
+    db_client.create_database("keys")
+    db_client.create_database("alerts")
+    db_client.create_database("logs")
+
 
 def init_app(app):
+    init_db()
     app.teardown_appcontext(close_db)
 
--- logger/model.py
+++ logger/model.py
@@ -6,27 +6,32 @@ from .dashboard import get_dashboard_json
 import json
 import requests
 
+
 def insert_alert(json_data):
     # vdc_name, app_name, status, category, message, level, last_occurance
     client = get_db()
-    client.switch_database('logs')
-    tname = json_data.get('tname')
-    explorer_url = json_data.get('explorer_url')
-    level = json_data.get('level')
-    vdc_name = json_data.get('vdc_name')
-    app_name = json_data.get('app_name')
-    status = json_data.get('status')
-    category = json_data.get('category')
-    type_ = json_data.get('type')
-    count = json_data.get('count')
-    message = json_data.get('message')
+    client.switch_database("logs")
+    tname = json_data.get("tname")
+    explorer_url = json_data.get("explorer_url")
+    level = json_data.get("level")
+    vdc_name = json_data.get("vdc_name")
+    app_name = json_data.get("app_name")
+    status = json_data.get("status")
+    category = json_data.get("category")
+    type_ = json_data.get("type")
+    count = json_data.get("count")
+    message = json_data.get("message")
     if (
-        tname is None or
-        level is None or vdc_name is None or
-        app_name is None or status is None or
-        category is None or type_ is None or
-        count is None or message is None
-        ):
+        tname is None
+        or level is None
+        or vdc_name is None
+        or app_name is None
+        or status is None
+        or category is None
+        or type_ is None
+        or count is None
+        or message is None
+    ):
         raise MissingValueException("some properties are not provided")
     points = [
         {
@@ -41,75 +46,64 @@ def insert_alert(json_data):
                 "category": category,
                 "type": type_,
             },
-            "fields": {
-                "message": message,
-                "count": count,
-            }
-        },
+            "fields": {"message": message, "count": count},
+        }
     ]
     client.write_points(points)
 
+
 def insert_heartbeat(json_data):
     client = get_db()
-    client.switch_database('logs')
-    tname = json_data.get('tname')
-    explorer_url = json_data.get('explorer_url')
-    vdc_name = json_data.get('vdc_name')
+    client.switch_database("logs")
+    tname = json_data.get("tname")
+    explorer_url = json_data.get("explorer_url")
+    vdc_name = json_data.get("vdc_name")
     if tname is None or vdc_name is None:
         raise MissingValueException("some properties are not provided")
     points = [
         {
             "measurement": "heartbeats",
-            "tags": {
-                "tname": tname,
-                "explorer_url": explorer_url,
-                "vdc_name": vdc_name
-            },
-            "fields": {
-                "exists": 1
-            }
-        },
+            "tags": {"tname": tname, "explorer_url": explorer_url, "vdc_name": vdc_name},
+            "fields": {"exists": 1},
+        }
     ]
     client.write_points(points)
+
 
 def get_verify_key_from_db(customer_tid, explorer_url):
     customer_tid = int(customer_tid)
     client = get_db()
-    client.switch_database('keys')
-    results = client.query('SELECT pubkey, tid, explorer_url FROM "keys"')
-    vals = results.get_points(tags={'tid': str(customer_tid), 'explorer_url': explorer_url})
+    client.switch_database("verify_keys")
+    results = client.query("SELECT pubkey, tid, explorer_url FROM verify_keys")
+    vals = results.get_points(tags={"tid": str(customer_tid), "explorer_url": explorer_url})
     try:
         key = next(vals)
         return binascii.unhexlify(key)
     except:
         return None
 
+
 def cache_verify_key(customer_tid, explorer_url, verify_key):
     customer_tid = int(customer_tid)
     verify_key = binascii.hexlify(verify_key).decode()
     client = get_db()
-    client.switch_database('keys')
+    client.switch_database("verify_keys")
     points = [
         {
-            "measurement": "keys",
-            "tags": {
-                "tid": customer_tid,
-                "explorer_url": explorer_url
-            },
-            "fields": {
-                "pubkey": verify_key
-            }
-        },
+            "measurement": "verify_keys",
+            "tags": {"tid": customer_tid, "explorer_url": explorer_url},
+            "fields": {"pubkey": verify_key},
+        }
     ]
     client.write_points(points)
 
 
 def add_new_dashboard(json_data):
     token = current_app.config["GRAFANA_KEY"]
-    tname = json_data.get('tname')
-    vdc_name = json_data.get('vdc_name')
+    tname = json_data.get("tname")
+    vdc_name = json_data.get("vdc_name")
     if tname is None or vdc_name is None:
         raise MissingValueException("some properties are not provided")
     dashboard_str = get_dashboard_json(tname, vdc_name)
-    hed = {'Authorization': 'Bearer ' + token}
+    hed = {"Authorization": "Bearer " + token}
     requests.post("http://localhost:3000/api/dashboards/db", json=json.loads(dashboard_str), headers=hed)
